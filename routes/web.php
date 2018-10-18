d6220d309ea8ecf32368220b50850571e558b16a
Bassem Moustafa <bassem@yahoo.com>
2018-10-18 22:11:36 +0200

    Created Model -a

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*
+1) We want to create a model for the products called Product, but we want to create migration, factory and resouce controller at the same time : php artisan make:model Model/Product -a
+2) Do the same thing with reviews : php artisan make:model Mode/Review -a
+3) Go to api.php file ctrl + p then type api.php
+4) Make the apiResource for the products and reviews then push everything on github :
+    git status then git add . then git status then git commit
+
+*/
